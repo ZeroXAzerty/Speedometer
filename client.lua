34a9b4544e850fc26954c8492dce0b31f5bdf0c4
ZeroXAzerty <87154152+ZeroXAzerty@users.noreply.github.com>
2021-11-06 04:36:45 +0100

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,46 @@
+function text(content)
+    SetTextFont(1)
+    SetTextProportional(0)
+    SetTextScale(1.2, 1.2)
+    SetTextEntry("STRING")
+    AddTextComponentString(content)
+    DrawText(0.87, 0.8)
+
+end
+function secondtext(content)
+    SetTextFont(1)
+    SetTextProportional(0)
+    SetTextScale(1.05, 1.05)
+    SetTextEntry("STRING")
+    AddTextComponentString(content)
+    DrawText(0.87, 0.9)
+
+end
+
+
+
+Citizen.CreateThread(function()
+    local run = false
+    local ped = PlayerPedId()
+    local speed = 0
+    while true do
+        Citizen.Wait(0)
+        if IsControlJustPressed(1, 303) then
+            run = true
+            while run == true do
+                Citizen.Wait(0)
+                if IsControlJustPressed(1, 303) then
+                    run = false
+
+                elseif (IsPedInAnyVehicle(ped, false)) then
+                    speed = GetEntitySpeed(GetVehiclePedIsIn(ped, false))*3.6
+                    text(math.floor(speed).." Km/h")
+
+                elseif (IsPedInAnyVehicle(ped, false)) == false then
+                    speed = GetEntitySpeed(ped)*3.6
+                    secondtext(math.floor(speed).." Km/h")
+                end
+            end
+        end
+    end
+end)
